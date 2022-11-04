3d53670f9a802fbaeaab488d3c7e504e03d2b181
Ismaelsrz2003 <ismaelsrz03@gmail.com>
2022-11-04 11:50:59 +0100

    commit creamos el constructor de la clase Ismael

--- src/Ismael.java
+++ src/Ismael.java
@@ -4,6 +4,12 @@ public class Ismael {
 	private int edad;
 	private String nacionalidad;
 	
+	public Ismael(int edad, String nacionalidad) {
+		super();
+		this.edad = edad;
+		this.nacionalidad = nacionalidad;
+	}
+	
 	
 	
 }
